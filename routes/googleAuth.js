d34d9a8a3c5ac019213a0439fc948f01d783a2b3
suryansh2121 <rajputsuryansh491@gmail.com>
2025-06-10 22:48:07 +0530

    callback urls setup and googleauth set

--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('../config/passport');
 const { generateToken } = require('../utils/jwt');
-const { frontendUrl } = require('../config/env');
+const { frontendURL } = require('../config/env');
 
 router.get('/', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
@@ -11,7 +11,7 @@ router.get(
   passport.authenticate('google', { session: false }),
   (req, res) => {
     const token = generateToken(req.user._id);
-    res.redirect(`${frontendUrl}/?token=${token}`);
+    res.redirect(`${frontendURL}/?token=${token}`);
   }
 );
 
