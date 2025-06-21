60bc402b3d16919b17e6821599e1e9bbf274cef9
suryansh2121 <rajputsuryansh491@gmail.com>
2025-06-21 12:54:47 +0530

    some changes for check

--- server.js
+++ server.js
@@ -1,48 +1,44 @@
-require('dotenv').config(); 
-
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const session = require('express-session');
-const passport = require('./config/passport');
-const { port, frontendURL } = require('./config/env');
-const authRoutes = require('./routes/authRoute');
-const googleAuthRoutes = require('./routes/googleAuth');
-const warrantyRoutes = require('./routes/productRoute');
-const { scheduleReminders } = require('./scheduler/mailScheduler');
-const connectDB = require('./config/db');
-const errorHandler = require('./middleware/errorHandler');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const { port, frontendURL } = require("./config/env");
+const authRoutes = require("./routes/authRoute");
+const googleAuthRoutes = require("./routes/googleAuth");
+const warrantyRoutes = require("./routes/productRoute");
+const { scheduleReminders } = require("./scheduler/mailScheduler");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 
 app.use(helmet());
-app.use(morgan('combined'));
-app.use(rateLimit({
+app.use(morgan("combined"));
+app.use(
+  rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100,
-}));
-app.use(cors({
+  })
+);
+app.use(
+  cors({
   origin: frontendURL,
   credentials: true,
-}));
+  })
+);
 
 app.use(express.json());
 
-
-
-
 connectDB();
 
-app.use('/api/auth', authRoutes);
-app.use('/api/auth/google', googleAuthRoutes);
-app.use('/api/warranty', warrantyRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/auth/google", googleAuthRoutes);
+app.use("/api/warranty", warrantyRoutes);
 
 app.use(errorHandler);
 
 scheduleReminders();
 
-app.listen(port, () =>
-    console.log(`Server running on port ${port}`)
-);
+app.listen(port, () => console.log(`Server running on port ${port}`));
