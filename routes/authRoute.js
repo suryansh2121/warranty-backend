ed0c178d364e0b78ae84006c08cfaa541dc644a0
suryansh2121 <rajputsuryansh491@gmail.com>
2025-06-29 06:39:17 +0530

    forgot password router added

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const auth = require('../middleware/auth')
 const authController = require('../controller/authcontroller');
 const user = require('../model/user');
+const ForgotPassword = require("./forgot-password")
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/me', auth , (req, res) =>{
     res.json({user: req.user});
 });
+router.post('/forgot-password', ForgotPassword);
 
 module.exports = router;
--- routes/forgot-password.js
+++ routes/forgot-password.js
@@ -1,23 +1,27 @@
-const crypto =  require("crypto");
-const User = require('../model/user');
-const {sendResetEmail} = require("../scheduler/sendResetEmail")
+const crypto = require("crypto");
+const User = require("../model/user");
+const { sendResetEmail } = require("../scheduler/sendResetEmail");
+const { frontendURL } = require("./config/env");
 
-
-
-const router = require("./authRoute");
-
-router.post('/forgot-password', async(req , res)=>{
-    const {email} = req.body;
-    const user  = await User.findOne({email});
-    if(!user) return res.status(404).json({message: 'user not found'});
+const ForgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
 
     const token = crypto.randomBytes(32).toString("hex");
     user.resetToken = token;
     user.resetTokenExpiry = Date.now() + 3600000;
     await user.save();
-    const link = `http://localhost:5173/reset-password/${token}`;
-    await sendResetEmail(user.email, link);
-    res.json({message: "Reset link sent to your email."});
 
-});
-module.exports = router
+    const link = `${frontendURL}/reset-password/${token}`; 
+    await sendResetEmail(user.email, link);
+
+    res.json({ message: "Reset link sent to your email." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+module.exports = ForgotPassword;
